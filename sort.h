b374d09ac6574743aa76a23adfeb1df947268163
Adebayo Samuel <eladebayoor@gmail.com>
2022-07-16 01:59:54 +0100

    [feat]: quick sort algorithm done

--- sort.h
+++ sort.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -24,5 +25,6 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
