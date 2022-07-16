7b6ca26fe07782c74507509be4a8e3d43e054d96
Adebayo Samuel <eladebayoor@gmail.com>
2022-07-16 02:04:17 +0100

    [style]: betty code style requirements

--- sort.h
+++ sort.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -15,9 +14,9 @@
  */
 typedef struct listint_s
 {
-  const int n;
-  struct listint_s *prev;
-  struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
